58a6790227452e958733f8336c589cc98a63d151
Madhav Verma <116595091+madhav-7781@users.noreply.github.com>
2023-01-08 15:30:29 +0530

    Update and rename README.md to guess-game.py

---
+++ guess-game.py
@@ -0,0 +1,18 @@
+import random as r
+lower = 0
+higher = 10
+answer = r.randint(lower,higher)
+print("Please guess number between {} to {}: ".format(lower,higher))
+guess_no = int(input())
+if guess_no == answer:
+    print("Congrat's you did it in first go")
+else:
+    if guess_no < answer:
+        print("Have one more try, you guessed higher")
+    else:
+        print("Have one more try, you guessed lower")
+    guess_no = int(input())
+    if guess_no == answer:
+        print("Well done you guessed it correct!")
+    else:
+        print("Sorry, you lose!")
